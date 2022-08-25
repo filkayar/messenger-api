cf1ddeed33ad6278dabd97d35f9106d590d4242f
Илья <ilya19xx2014@gmail.com>
2022-08-26 02:21:21 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/messenger/api/repositaries/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.example.messenger.api.repositaries
+
+import com.example.messenger.api.models.User
+import org.springframework.data.repository.CrudRepository
+
+interface UserRepository : CrudRepository<User, Long> {
+
+    fun findByUsername(username: String): User?
+    fun findByPhoneNumber(phoneNumber: String): User?
+}
